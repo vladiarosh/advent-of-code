aebfb84f727860819aca76769250d2adc2a13848
Vladislav Iaroshenko <vladyaroshenko07@gmail.com>
2024-05-12 01:41:41 +0300

    Added part 2 for day 1 and solution for day 2

--- 2015_1.py
+++ 2015_1.py
@@ -1,3 +1,4 @@
+# PART ONE
 # version 1 with O(2n)
 file = open('2015_1_input.txt')
 my_instructions = file.read()
@@ -14,3 +15,14 @@ for char in my_instructions:
     else:
         floor_total_count -= 1
 print("Your floor is", floor_total_count)
+
+# PART TWO, learning how enumerator works, writing conditions in for loops correctly, breaking for loops
+floor_total_count = 0
+for i, char in enumerate(my_instructions, 1):
+    if char == '(':
+        floor_total_count += 1
+    else:
+        floor_total_count -= 1
+        if floor_total_count == -1:
+            print("Position of the basement floor is", i)
+            break
